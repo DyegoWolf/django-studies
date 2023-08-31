d9bcae443f220bb8d605e2a5b30a0990e2cd1ea7
Dyego Magno <dyegowolf@gmail.com>
2023-08-30 23:00:24 -0300

    Create templates index and contato

--- studies.venv/core/views.py
+++ studies.venv/core/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 
 #Renderiza a página html que será acessada mediante uma requisição
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'nome': 'Dyego',
+    }
+
+    return render(request, 'index.html', context)
 
 def contact(request):
     return render(request, 'contato.html')
