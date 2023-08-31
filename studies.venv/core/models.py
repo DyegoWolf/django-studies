b73236288c3f78214ad0759d2403238a49b16feb
Dyego Magno <dyegowolf@gmail.com>
2023-08-31 00:28:17 -0300

    Create models Produto and Cliente. Also create migrations

--- studies.venv/core/models.py
+++ studies.venv/core/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    nome = models.CharField('Nome', max_length=400)
+    preco = models.DecimalField('Preço', decimal_places=2, max_digits=8)
+    estoque = models.IntegerField('Quantidade em estoque')
