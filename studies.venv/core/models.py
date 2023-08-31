ddde20c6b06ee9f8d76fe8500ea11093ee5212ff
Dyego Magno <dyegowolf@gmail.com>
2023-08-31 00:29:29 -0300

    Alter model Cliente

--- studies.venv/core/models.py
+++ studies.venv/core/models.py
@@ -4,3 +4,8 @@ class Produto(models.Model):
     nome = models.CharField('Nome', max_length=400)
     preco = models.DecimalField('Preço', decimal_places=2, max_digits=8)
     estoque = models.IntegerField('Quantidade em estoque')
+
+class Cliente(models.Model):
+    nome = models.CharField('Nome', max_length=400)
+    sobrenome = models.CharField('Sobrenome', max_length=400)
+    email = models.EmailField('Email', max_length=100)
