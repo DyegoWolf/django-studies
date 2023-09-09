cb6c53a7c5c6c2e84f6937ec4536c79ef276fbab
Dyego Magno <dyegowolf@gmail.com>
2023-09-09 15:04:31 -0300

    Identify logged in user

--- studies.venv/core/views.py
+++ studies.venv/core/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 #Renderiza a página html que será acessada mediante uma requisição
 def index(request):
+
+    # Verifica se o usuário está logado
     if str(request.user) == 'AnonymousUser':
         teste = 'Usuário não logado'
     else:
