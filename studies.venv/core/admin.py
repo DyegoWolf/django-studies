446eec5b427752eb01c2db6fd69aa05508885f20
Dyego Magno <dyegowolf@gmail.com>
2023-09-09 09:48:53 -0300

    Configures django admin

--- studies.venv/core/admin.py
+++ studies.venv/core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Cliente, Produto
 
-# Register your models here.
+# Classe que contém todos os atributos que serão listados na tabela do painel administrativo
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'preco', 'estoque')
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'sobrenome', 'email')
+
+# Arquivo para registro das models que serão listadas no painel administrativo
+admin.site.register(Cliente, ClienteAdmin)
+admin.site.register(Produto, ProdutoAdmin)
--- studies.venv/core/models.py
+++ studies.venv/core/models.py
@@ -5,7 +5,14 @@ class Produto(models.Model):
     preco = models.DecimalField('Preço', decimal_places=2, max_digits=8)
     estoque = models.IntegerField('Quantidade em estoque')
 
+    # Método que define as colunas que serão apresentadas na tabela do painel administrativo
+    def __str__(self):
+        return (self)
+
 class Cliente(models.Model):
     nome = models.CharField('Nome', max_length=400)
     sobrenome = models.CharField('Sobrenome', max_length=400)
     email = models.EmailField('Email', max_length=100)
+
+    def __str__(self):
+        return (self.nome)
